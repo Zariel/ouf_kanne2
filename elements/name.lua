1290b1610d4f8c0613d41dbf788074a538c718d2
Chris Bannister <c.bannister@gmail.com>
2011-01-25 16:00:17 +0000

    Really ..

--- elements/name.lua
+++ elements/name.lua
@@ -3,20 +3,23 @@ local layout = ns.layout
 
 local update = function(self, event, unit)
 	if(self.unit ~= unit) then return end
-	local Name = self.Name
-	if(Name.OverideUpdateName) then
-		Name:OverideUpdateName(event, unit)
-	else
-		Name:SetText(UnitName(unit))
-	end
+	self.Name:SetText(UnitName(unit))
 end
 
 local path = function(self, ...)
 	return (self.Name.Override or update)(self, ...)
 end
 
+local ForceUpdate = function(self)
+	return path(self.__owner, "ForceUpdate", self.__owner.unit)
+end
+
 local enable = function(self, unit)
-	if(self.Name) then
+	local name = self.Name
+	if(name) then
+		name.__owner = self
+		name.ForceUpdate = ForceUpdate
+
 		self:RegisterEvent("UNIT_NAME_UPDATE", path)
 
 		return true
@@ -25,7 +28,7 @@ end
 
 local disable = function(self)
 	if(self.Name) then
-		self:UnregisterEvent("UNIT_NAME_UPDATE")
+		self:UnregisterEvent("UNIT_NAME_UPDATE", path)
 	end
 end
 
